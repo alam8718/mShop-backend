c5febf3e1eb7886ffa4a70667db9ecc4e0a98dea
Mohammad Mahabub Alam <mahabubalam3004@gmail.com>
2024-08-13 18:26:04 +0600

    config middleware changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,9 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['*'],
+      origin: ['*'], 
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
   },
   'strapi::poweredBy',
